e292d3599384d56a9539d7a0a4a82167318f58ad
David Harrison <watchinharrison@me.com>
2015-06-09 14:51:44 +0100

    Added Example Directory to Test Module

--- angular-cloud-kit.js
+++ angular-cloud-kit.js
@@ -6,7 +6,7 @@
 
 'use strict';
 
-angular.module('ngCloudKit', []).provider('$cloudKit', function() {
+angular.module('cloudKit', []).provider('$cloudKit', function() {
     var module          = this;
 
 
